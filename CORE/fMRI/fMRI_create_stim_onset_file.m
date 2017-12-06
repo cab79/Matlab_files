40b4a49c76e83234a0a013d89c96b89300800c29
cab79 <cb802@cam.ac.uk>
2017-12-06 10:46:00 +0000

    up

--- CORE/fMRI/fMRI_create_stim_onset_file.m
+++ CORE/fMRI/fMRI_create_stim_onset_file.m
@@ -1,7 +1,7 @@
 %% settings
 studyname = 'CORE';
 subnum = 'pilot';
-dname = 'C:\Matlab_files\CORE\fMRI\SCIn_ouputs';
+dname = 'C:\Matlab_files\CORE\fMRI\SCIn_outputs';
 seq_fname = 'Sequence_pilot_Sequence_CORE_fMRI_cont_8block_OptionfMRI_run_startblock1_20171024T104134.mat';
 out_fname = 'Output_pilot_Sequence_CORE_fMRI_cont_8block_OptionfMRI_run_startblock1_20171024T104134.mat';
 
@@ -10,24 +10,36 @@ out_fname = 'Output_pilot_Sequence_CORE_fMRI_cont_8block_OptionfMRI_run_startblo
 onset_marker = 'expected';
 correct_missing = 1; % if there are missing stim onsets, infer what they should be. Number here is estimated ISI. Only works if ISI is fixed throughout expt.
 
-% event condition names and their numbers
+% event condition names, "condition" numbers, and "signal" numbers of interest
 events = {
-    'tact_slow_odd',1
-    %'tact_slow_stan',2
-    'aud_slow_odd',3
-    %'aud_slow_stan',4
-    'tact_fast_odd',5
-    %'tact_fast_stan',6
-    'aud_fast_odd',7
-    %'aud_fast_stan',8
+    'tact_slow_odd',1,[1 2]
+    'aud_slow_odd',3,[3 4]
+    'tact_fast_odd',5,[1 2]
+    'aud_fast_odd',7,[3 4]
+    'tact_slow_odd_chan1',1,[1]
+    'aud_slow_odd_chan1',3,[3]
+    'tact_fast_odd_chan1',5,[1]
+    'aud_fast_odd_chan1',7,[3]
+    'tact_slow_odd_chan2',1,[2]
+    'aud_slow_odd_chan2',3,[4]
+    'tact_fast_odd_chan2',5,[2]
+    'aud_fast_odd_chan2',7,[4]
     };
 
 % block names and associated condition numbers
 blocks = {
-    'tact_slow',[1 2]
-    'aud_slow',[3 4]
-    'tact_fast',[5 6]
-    'aud_fast',[7 8]
+    'tact_slow',[1 2],[1 2]
+    'aud_slow',[3 4],[3 4]
+    'tact_fast',[5 6],[1 2]
+    'aud_fast',[7 8],[3 4]
+    %'tact_slow_chan1',[1 2],[1]
+    %'aud_slow_chan1',[3 4],[3]
+    %'tact_fast_chan1',[5 6],[1]
+    %'aud_fast_chan1',[7 8],[3]
+    %'tact_slow_chan2',[1 2],[2]
+    %'aud_slow_chan2',[3 4],[4]
+    %'tact_fast_chan2',[5 6],[2]
+    %'aud_fast_chan2',[7 8],[4]
     };
 blockmarker = 0;
 
@@ -64,18 +76,18 @@ switch onset_marker
         stimtime = [0 cumsum(expisi)] + rectime(1)-scanstart;
 end
 
-for i = 1:size(events,1)
-    conds = ismember(seq.condnum,events{i,2});
+for i = 1:size(events,1) 
+    conds = ismember(seq.condnum,events{i,2}) & ismember(seq.signal,events{i,3});
     onsets = stimtime(conds)';
     
     %save([events{i,1} '.txt'],'onsets','-ASCII')
-    fid = fopen([studyname subnum '_' events{i,1} '_event.txt'], 'wt');
+    fid = fopen(fullfile(dname,[studyname subnum '_' events{i,1} '_event.txt']), 'wt');
     fprintf(fid, '%.2f\n', onsets);
     fclose(fid);
 end
 
 for i = 1:size(blocks,1)
-    conds = ismember(seq.condnum,blocks{i,2});
+    conds = ismember(seq.condnum,blocks{i,2}) & ismember(seq.signal,blocks{i,3});
     st=find(diff(conds)==1); % don't add 1 because we will start from the first '0' condition
     en=find(diff(conds)==-1)+1;
     
@@ -87,11 +99,11 @@ for i = 1:size(blocks,1)
     onsets = stimtime(st)';
     durations = stimtime(en)'-stimtime(st)';
     
-    fid = fopen([studyname subnum '_' blocks{i,1} '_block.txt'], 'wt');
+    fid = fopen(fullfile(dname,[studyname subnum '_' blocks{i,1} '_block.txt']), 'wt');
     fprintf(fid, '%.2f\n', onsets);
     fclose(fid);
     
-    fid = fopen([studyname subnum '_' blocks{i,1} '_block_dur.txt'], 'wt');
+    fid = fopen(fullfile(dname,[studyname subnum '_' blocks{i,1} '_block_dur.txt']), 'wt');
     fprintf(fid, '%.2f\n', durations);
     fclose(fid);
 end
